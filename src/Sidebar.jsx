b8939cc23e56bfaedfffdf4c05baf5bd64ffb5ea
sophienocode <sophieprotanki@gmail.com>
2026-03-18 23:01:27 -0300

    (Auth) casual update

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -10,6 +10,9 @@ function Sidebar() {
     <h1>Olá , como vai o dia ?</h1>
     <h1>Olá , como vai o dia ?</h1>
     <h1>Olá , como vai o dia ?</h1>
+    <h1>Olá , como vai o dia ?</h1>
+    <h1>Olá , como vai o dia ?</h1>
+    <h1>Olá , como vai o dia ?</h1>
 
     </>
   );
