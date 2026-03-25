1fdb97d6801931c4a37ebd985c6426d266672e90
sophienocode <sophieprotanki@gmail.com>
2026-03-24 23:46:18 -0300

    (Auth):

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -7,13 +7,6 @@ function Sidebar() {
     <h1>Olá , como vai o dia ?</h1>
     <h1>Olá , como vai o dia ?</h1>
     <h1>Olá , como vai o dia ?</h1>
-
-
-    <h1>Olá , como vai o dia ?</h1>
-    <h1>Olá , como vai o dia ?</h1>
-    <h1>Olá , como vai o dia ?</h1>
-
-    <h1>Olá , como vai o dia ?</h1>
     <h1>Olá , como vai o dia ?</h1>
     <h1>Olá , como vai o dia ?</h1>
 
